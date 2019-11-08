de53d4da57db593e3fbc0e1109f9037165e34478
lgn <lgnsg@foxmail.com>
2019-11-08 18:42:59 +0800

    add: add lazy function

--- src/packages/lazyLoadImg/main.js
+++ src/packages/lazyLoadImg/main.js
@@ -10,10 +10,19 @@ function Lazy (el, binding) {
   if (!(this instanceof Lazy)) return new Lazy(el, binding) // 防止忘记用new 创建实例
   this.el = el // 公有属性
   this.binding = binding
+  function hasIntersectionObserver () { // 私有属性,实例无法获取
+    if ('IntersectionObserver' in window &&
+      'IntersectionObserverEntry' in window &&
+      'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+      return true
+    }
+    return false
+  }
   this.judgeEnv = function () {
     // 公有方法
     let self = this
-    if ('IntersectionObserver' in window) {
+    // console.log(`\u001b[3${~~(Math.random() * 8)}m${hasIntersectionObserver()}\u001b[0m`)
+    if (hasIntersectionObserver()) {
       self.observer = null
       self.useIntersectionObserver(el, binding)
     } else {
